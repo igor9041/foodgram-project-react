cecc480b77fe874d3f7aeeff2337aac9e86e99fc
Igor Malgin <78731047+igor9041@users.noreply.github.com>
2022-02-05 18:05:17 +0300

    update

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,7 +1,23 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
 from foodgram.settings import EMPTY_STRING_FOR_ADMIN_PY
 
 from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
                             ShoppingCart, Tag)
 
+
+@register(Recipe)
+class RecipeAdmin(ModelAdmin):
+    list_display = ('author', 'name', 'favorited')
+    search_fields = ('author', 'name',)
+    list_filter = ('name', 'author', 'tags')
+    
+    def favorited(self, obj):
+        return Favorite.objects.filter(recipe=obj).count()
+
+
+@register(Ingredient)
+class RecipeAdmin(ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('author', 'name',)
+    list_filter = ('name',)
